a470e1e215b9909b4c92a6160aabe6e1bd4303de
costub28 <113878151+costub28@users.noreply.github.com>
2022-11-23 10:10:11 +0900

    Update distAlert.py

--- wasd/distAlert.py
+++ wasd/distAlert.py
@@ -3,7 +3,7 @@ import winsound
 D = 100
 max_distance = 100
 
-//거리별로 70,80,90%에 해당하는 구역에 도달할시 3단계을 알람을 단계별로 울리게 하였다.
+#거리별로 70,80,90%에 해당하는 구역에 도달할시 3단계을 알람을 단계별로 울리게 하였다.
 def distAlert():
     if (D >= float(max_distance)*0.7 and D < float(max_distance)*0.8):
         winsound.Beep(500, 1200)
